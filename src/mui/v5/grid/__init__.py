9771f521a16212cb15ac275150e8c1c681e68c58
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-10-21 13:35:12 -0400

    refactor: expose types higher in the import path

--- src/mui/v5/grid/__init__.py
+++ src/mui/v5/grid/__init__.py
@@ -1,13 +1,15 @@
 from mui.v5.grid.filter import GridFilterItem, GridFilterModel
 from mui.v5.grid.link import GridLinkOperator
-from mui.v5.grid.sort import GridSortDirection, GridSortItem, GridSortModel
+from mui.v5.grid.sort import Field, GridSortDirection, GridSortItem, GridSortModel, Sort
 
 # isort: unique-list
 __all__ = [
+    "Field",
     "GridFilterItem",
     "GridFilterModel",
     "GridLinkOperator",
     "GridSortDirection",
     "GridSortItem",
     "GridSortModel",
+    "Sort",
 ]
